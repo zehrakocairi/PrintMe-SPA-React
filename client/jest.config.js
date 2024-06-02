9294d4cd6e2fb2df6838704be1e6e143dc920b98
zekeriya kocairi <zkocairi@skillstown.com>
2024-06-02 12:00:27 +0200

    some cleanup

--- client/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["./setupTests.js"],
-  moduleNameMapper: {
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
-    "\\.(jpg|jpeg|png|svg)$": "<rootDir>/__mocks__/fileMock.js",
-  },
-};
