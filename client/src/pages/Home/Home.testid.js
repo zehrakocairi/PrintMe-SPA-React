f181d44c0dea5d4f9162cc94db80072dec08f8cd
zekeriya kocairi <zkocairi@skillstown.com>
2024-06-02 12:04:37 +0200

    clean everything

--- client/src/pages/Home/Home.testid.js
+++
@@ -1,7 +0,0 @@
-import createTestIdFilePath from "../../utils/createTestIdFilePath";
-
-const TEST_ID = {
-  container: `${createTestIdFilePath("pages", "Home", "Home")}-container`,
-};
-
-export default TEST_ID;
