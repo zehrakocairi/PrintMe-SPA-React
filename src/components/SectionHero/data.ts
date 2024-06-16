e73502e833a2d79edfd2ad7d35ff0c5f79e0ed61
zekeriya kocairi <zkocairi@skillstown.com>
2024-06-16 21:20:58 +0200

    some changes

--- src/components/SectionHero/data.ts
+++ src/components/SectionHero/data.ts
@@ -7,13 +7,7 @@ interface Hero2DataType {
 }
 const image1 = 'https://genstorageaccount3116.blob.core.windows.net/printme-images/main-slider-1.jpg'
 const image2 = 'https://genstorageaccount3116.blob.core.windows.net/printme-images/main-slider-3.jpg'
-const image3 = 'https://genstorageaccount3116.blob.core.windows.net/printme-images/main-slider-4.jpg'
-// const image1 = 'https://images.unsplash.com/photo-1569084024058-1632922a4e1d?q=80&w=2519&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-// const image1 = 'https://images.unsplash.com/photo-1574514120529-364d014b9a0a?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
-
-// const image1 = 'https://wallpapers.com/images/high/bouquet-of-flowers-famous-painting-se4ude7nwqzjp7uf.webp'
-// const image2 = 'https://genstorageaccount3116.blob.core.windows.net/printme-images/pexels-axp-photography-500641970-17377188.jpg' 
-// const image3 = 'https://genstorageaccount3116.blob.core.windows.net/printme-images/pexels-ekrulila-18879508.jpg' 
+const image3 = 'https://genstorageaccount3116.blob.core.windows.net/printme-images/main-slider-4.jpg' 
 
 export const HERO2_DEMO_DATA: Hero2DataType[] = [
   {
