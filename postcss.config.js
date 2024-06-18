aa661b98bfd03772305022ae12afe3fe9fded616
zekeriya kocairi <zkocairi@skillstown.com>
2024-06-18 02:13:39 +0200

    lazy load the pages

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    purgecss({
+      content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    }),
+  ],
+};
