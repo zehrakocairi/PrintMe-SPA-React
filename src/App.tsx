34ec985d0828693750c7abcc9211e97ad27972a1
zekeriya kocairi <zkocairi@skillstown.com>
2024-08-20 19:29:20 +0200

    Revert "Enable tracing back"
    This reverts commit 27c2cb9404c16f847219a3ffc93b352cd0424f63.

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { FilterProvider } from "./contexts/FilterContext";
 import { ApplicationProvider } from "./contexts/ApplicationContext";
 import { CartProvider } from "./contexts/CartContext";
 import CommonClient from "./pages/CommonClient";
-import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
-import { reactPlugin } from "./services/applicationInsightService";
+// import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
+// import { reactPlugin } from "./services/applicationInsightService";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n/i18n";
 import { Suspense, lazy } from "react";
@@ -34,44 +34,44 @@ import { LazyMotion, domAnimation } from "framer-motion"
 
 function App() {
   return (
-    <AppInsightsContext.Provider value={reactPlugin}>
-      <ApplicationProvider>
-        <CartProvider>
-          <I18nextProvider i18n={i18n}>
-            <LazyMotion features={domAnimation}>
-              <Router>
-                <SiteHeader />
-                <div className="content-container">
-                  <FilterProvider>
-                    <Suspense fallback={<div>Loading...</div>}>
-                      <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<PageAbout />} />
-                        <Route path="/your-design" element={<PageYourDesign />} />
-                        <Route path="/cart" element={<CartPage />} />
-                        <Route path="/checkout" element={<CheckoutPage />} />
-                        <Route path="/search" element={<PageSearch />} />
-                        <Route path="/product-detail/:id" element={<ProductDetailPage />} />
-                        <Route path="/contact" element={<PageContact />} />
-                        <Route path="/our-services" element={<PageOurServices />} />
-                        <Route path="/account" element={<CommonLayout />} >
-                          <Route path="account-details" element={<AccountPage />} />
-                          <Route path="account-billing" element={<AccountBilling />} />
-                          <Route path="account-order" element={<AccountOrder />} />
-                        </Route>
-                        <Route path="/login" element={<PageLogin />} />
-                      </Routes>
-                    </Suspense>
-                    <CommonClient />
-                    <Footer />
-                  </FilterProvider>
-                </div>
-              </Router>
-            </LazyMotion>
-          </I18nextProvider>
-        </CartProvider>
-      </ApplicationProvider>
-    </AppInsightsContext.Provider>
+    // <AppInsightsContext.Provider value={reactPlugin}>
+    <ApplicationProvider>
+      <CartProvider>
+        <I18nextProvider i18n={i18n}>
+          <LazyMotion features={domAnimation}>
+            <Router>
+              <SiteHeader />
+              <div className="content-container">
+                <FilterProvider>
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/about" element={<PageAbout />} />
+                      <Route path="/your-design" element={<PageYourDesign />} />
+                      <Route path="/cart" element={<CartPage />} />
+                      <Route path="/checkout" element={<CheckoutPage />} />
+                      <Route path="/search" element={<PageSearch />} />
+                      <Route path="/product-detail/:id" element={<ProductDetailPage />} />
+                      <Route path="/contact" element={<PageContact />} />
+                      <Route path="/our-services" element={<PageOurServices />} />
+                      <Route path="/account" element={<CommonLayout />} >
+                        <Route path="account-details" element={<AccountPage />} />
+                        <Route path="account-billing" element={<AccountBilling />} />
+                        <Route path="account-order" element={<AccountOrder />} />
+                      </Route>
+                      <Route path="/login" element={<PageLogin />} />
+                    </Routes>
+                  </Suspense>
+                  <CommonClient />
+                  <Footer />
+                </FilterProvider>
+              </div>
+            </Router>
+          </LazyMotion>
+        </I18nextProvider>
+      </CartProvider>
+    </ApplicationProvider>
+    // </AppInsightsContext.Provider>
   );
 }
 
