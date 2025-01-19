4667b67a1a9bc766d1ef049623ea140069f4a879
zekeriya kocairi <zkocairi@skillstown.com>
2025-01-19 18:39:48 +0100

    minor change

--- src/shared/Navigation/NavigationItem.tsx
+++ src/shared/Navigation/NavigationItem.tsx
@@ -1,5 +1,3 @@
-
-
 import { Popover, Transition } from "../../headlessui";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import CardCategory3 from "../../components/CardCategories/CardCategory3";
@@ -25,7 +23,6 @@ const NavigationItem: FC<NavigationItemProps> = ({ menuItem }) => {
   const [menuCurrentHovers, setMenuCurrentHovers] = useState<string[]>([]);
   const { t } = useTranslation();
 
-
   const onMouseEnterMenu = (id: string) => {
     setMenuCurrentHovers((state) => [...state, id]);
   };
@@ -45,9 +42,7 @@ const NavigationItem: FC<NavigationItemProps> = ({ menuItem }) => {
     }
     const isMobile = window.innerWidth < 768;
     return (
-      <li
-        className={`menu-item flex-shrink-0 menu-megamenu menu-megamenu--large`}
-      >
+      <li className={`menu-item flex-shrink-0 menu-megamenu menu-megamenu--large`}>
         {renderMainItem(menu)}
         <div className="invisible sub-menu absolute top-full inset-x-0 transform z-50">
           <div className="bg-white dark:bg-neutral-900 shadow-lg">
@@ -56,22 +51,12 @@ const NavigationItem: FC<NavigationItemProps> = ({ menuItem }) => {
                 <div className="flex-1 grid grid-cols-4 gap-6 xl:gap-8 pr-6 xl:pr-8">
                   {menu.children.map((item, index) => (
                     <div key={index}>
-                      <p className="font-medium text-slate-900 dark:text-neutral-200">
-                        {t(item.name)}
-                      </p>
-                      <ul className="grid space-y-4 mt-4">
-                        {item.children?.map(renderMegaMenuNavlink)}
-                      </ul>
+                      <p className="font-medium text-slate-900 dark:text-neutral-200">{t(item.name)}</p>
+                      <ul className="grid space-y-4 mt-4">{item.children?.map(renderMegaMenuNavlink)}</ul>
                     </div>
                   ))}
                 </div>
-                <div className="w-[40%] xl:w-[35%]">
-                  {!isMobile && (<CardCategory3  
-                  name={t('Visit Our Space')}
-                  desc={t('Pick up prints,<br />enjoy coffee')}
-                 featuredImage="https://genstorageaccount3116.blob.core.windows.net/printme-images/coffee-invitation.webp"/>)}
-                  
-                </div>
+                <div className="w-[40%] xl:w-[35%]">{!isMobile && <CardCategory3 name={t("Visit Our Space")} desc={t("Pick up prints,<br />enjoy coffee")} featuredImage="https://genstorageaccount3116.blob.core.windows.net/printme-images/coffee-invitation.webp" />}</div>
               </div>
             </div>
           </div>
@@ -83,10 +68,7 @@ const NavigationItem: FC<NavigationItemProps> = ({ menuItem }) => {
   const renderMegaMenuNavlink = (item: NavItemType) => {
     return (
       <li key={item.id} className={`${item.isNew ? "menuIsNew" : ""}`}>
-        <Link
-          className="font-normal text-slate-600 hover:text-black dark:text-slate-400 dark:hover:text-white "
-          href={ item.href}
-        >
+        <Link className="font-normal text-slate-600 hover:text-black dark:text-slate-400 dark:hover:text-white " href={item.href}>
           {t(item.name)}
         </Link>
       </li>
@@ -97,31 +79,12 @@ const NavigationItem: FC<NavigationItemProps> = ({ menuItem }) => {
   const renderDropdownMenu = (menuDropdown: NavItemType) => {
     const isHover = menuCurrentHovers.includes(menuDropdown.id);
     return (
-      <Popover
-        as="li"
-        className="menu-item menu-dropdown relative"
-        onMouseEnter={() => onMouseEnterMenu(menuDropdown.id)}
-        onMouseLeave={() => onMouseLeaveMenu(menuDropdown.id)}
-      >
+      <Popover as="li" className="menu-item menu-dropdown relative" onMouseEnter={() => onMouseEnterMenu(menuDropdown.id)} onMouseLeave={() => onMouseLeaveMenu(menuDropdown.id)}>
         {() => (
           <>
-            <Popover.Button as={Fragment}>
-              {renderMainItem(menuDropdown)}
-            </Popover.Button>
-            <Transition
-              as={Fragment}
-              show={isHover}
-              enter="transition ease-out duration-150"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel
-                static
-                className="sub-menu absolute transform z-10 w-56 top-full left-0"
-              >
+            <Popover.Button as={Fragment}>{renderMainItem(menuDropdown)}</Popover.Button>
+            <Transition as={Fragment} show={isHover} enter="transition ease-out duration-150" enterFrom="opacity-0 translate-y-1" enterTo="opacity-100 translate-y-0" leave="transition ease-in duration-150" leaveFrom="opacity-100 translate-y-0" leaveTo="opacity-0 translate-y-1">
+              <Popover.Panel static className="sub-menu absolute transform z-10 w-56 top-full left-0">
                 <ul className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10 text-sm relative bg-white dark:bg-neutral-900 py-4 grid space-y-1">
                   {menuDropdown.children?.map((i) => {
                     if (i.type) {
@@ -146,32 +109,12 @@ const NavigationItem: FC<NavigationItemProps> = ({ menuItem }) => {
   const renderDropdownMenuNavlinkHasChild = (item: NavItemType) => {
     const isHover = menuCurrentHovers.includes(item.id);
     return (
-      <Popover
-        as="li"
-        key={item.id}
-        className="menu-item menu-dropdown relative px-2"
-        onMouseEnter={() => onMouseEnterMenu(item.id)}
-        onMouseLeave={() => onMouseLeaveMenu(item.id)}
-      >
+      <Popover as="li" key={item.id} className="menu-item menu-dropdown relative px-2" onMouseEnter={() => onMouseEnterMenu(item.id)} onMouseLeave={() => onMouseLeaveMenu(item.id)}>
         {() => (
           <>
-            <Popover.Button as={Fragment}>
-              {renderDropdownMenuNavlink(item)}
-            </Popover.Button>
-            <Transition
-              as={Fragment}
-              show={isHover}
-              enter="transition ease-out duration-150"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel
-                static
-                className="sub-menu absolute z-10 w-56 left-full pl-2 top-0"
-              >
+            <Popover.Button as={Fragment}>{renderDropdownMenuNavlink(item)}</Popover.Button>
+            <Transition as={Fragment} show={isHover} enter="transition ease-out duration-150" enterFrom="opacity-0 translate-y-1" enterTo="opacity-100 translate-y-0" leave="transition ease-in duration-150" leaveFrom="opacity-100 translate-y-0" leaveTo="opacity-0 translate-y-1">
+              <Popover.Panel static className="sub-menu absolute z-10 w-56 left-full pl-2 top-0">
                 <ul className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10 text-sm relative bg-white dark:bg-neutral-900 py-4 grid space-y-1">
                   {item.children?.map((i) => {
                     if (i.type) {
@@ -195,17 +138,9 @@ const NavigationItem: FC<NavigationItemProps> = ({ menuItem }) => {
 
   const renderDropdownMenuNavlink = (item: NavItemType) => {
     return (
-      <Link
-        className="flex items-center font-normal text-neutral-6000 dark:text-neutral-400 py-2 px-4 rounded-md hover:text-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-800 dark:hover:text-neutral-200"
-        href={item.href}
-      >
+      <Link className="flex items-center font-normal text-neutral-6000 dark:text-neutral-400 py-2 px-4 rounded-md hover:text-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-800 dark:hover:text-neutral-200" href={item.href}>
         {item.name}
-        {item.type && (
-          <ChevronDownIcon
-            className="ml-2 h-4 w-4 text-neutral-500"
-            aria-hidden="true"
-          />
-        )}
+        {item.type && <ChevronDownIcon className="ml-2 h-4 w-4 text-neutral-500" aria-hidden="true" />}
       </Link>
     );
   };
@@ -213,27 +148,19 @@ const NavigationItem: FC<NavigationItemProps> = ({ menuItem }) => {
   // ===================== MENU MAIN MENU =====================
   const renderMainItem = (item: NavItemType) => {
     const isYourDesingLink = item.name === "Your Design";
-    const linkInLineStyle = isYourDesingLink ? {
-      color: "#db2877",
-      "fontFamily": "fantasy",
-      "fontSize": "1.09rem",
-      "fontWeight": "900",
-    } : {};
+    const linkInLineStyle = isYourDesingLink
+      ? {
+          color: "#db2877",
+          fontFamily: "fantasy",
+          fontSize: "1.09rem",
+          fontWeight: "200",
+        }
+      : {};
     return (
       <div className="h-20 flex-shrink-0 flex items-center">
-        <Link
-          style={linkInLineStyle}
-          className="inline-flex items-center text-sm lg:text-[15px] font-medium text-slate-700 dark:text-slate-300 py-2.5 px-4 xl:px-5 rounded-full hover:text-slate-900 hover:bg-slate-100 dark:hover:bg-slate-800 dark:hover:text-slate-200"
-          href={item.href}
-          aria-label={t(item.name)}
-        >
+        <Link style={linkInLineStyle} className="inline-flex items-center text-sm lg:text-[15px] font-medium text-slate-700 dark:text-slate-300 py-2.5 px-4 xl:px-5 rounded-full hover:text-slate-900 hover:bg-slate-100" href={item.href} aria-label={t(item.name)}>
           {t(item.name)}
-          {item.type && (
-            <ChevronDownIcon
-              className="ml-1 -mr-1 h-4 w-4 text-slate-400"
-              aria-hidden="true"
-            />
-          )}
+          {item.type && <ChevronDownIcon className="ml-1 -mr-1 h-4 w-4 text-slate-400" aria-hidden="true" />}
         </Link>
       </div>
     );
@@ -245,9 +172,7 @@ const NavigationItem: FC<NavigationItemProps> = ({ menuItem }) => {
     case "megaMenu":
       return renderMegaMenu(menuItem);
     default:
-      return (
-        <li className="menu-item flex-shrink-0">{renderMainItem(menuItem)}</li>
-      );
+      return <li className="menu-item flex-shrink-0">{renderMainItem(menuItem)}</li>;
   }
 };
 
